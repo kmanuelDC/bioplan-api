cb1ac9b306113f1f9ca87d4967231a7a7f87f44a
HaroldUCSM <haroldosorio32@gmail.com>
2023-03-09 20:45:56 -0500

    se realizo las APIs de colaboradores

--- app/controllers/colaboradores.controller.js
+++ app/controllers/colaboradores.controller.js
@@ -1,18 +1,55 @@
-import { BaseController } from '../core/base.controller.js';
-import ColaboradoresRepository from '../repository/colaboradores.respository.js';
-
+import { BaseController } from "../core/base.controller.js";
+import ColaboradoresRepository from "../repository/colaboradores.respository.js";
 
 export default class Colaboradores extends BaseController {
-    constructor() { super(); }
+  constructor() {
+    super();
+  }
 
-    async getColaboradores(req, res) {
-        try {
-            let response = await ColaboradoresRepository.getColaboradores()
-            return super.responseSuccess(req, res, response)
-        } catch (error) {
-            return super.responseError(req, res, error)
-
-        }
+  async getColaboradores(req, res) {
+    try {
+      let response = await ColaboradoresRepository.getColaboradores();
+      return super.responseSuccess(req, res, response);
+    } catch (error) {
+      return super.responseError(req, res, error);
     }
+  }
+  async getColaboradoById(req, res) {
+    try {
+      const id = req.params.id;
+      let response = await ColaboradoresRepository.getColaboradoById(id);
+      return super.responseSuccess(req, res, response);
+    } catch (error) {
+      return super.responseError(req, res, error);
+    }
+  }
 
+  async postColaborador(req, res) {
+    try {
+      const body = req.body;
+      let response = await ColaboradoresRepository.postColaborador(body);
+      return super.responseSuccess(req, res, response);
+    } catch (error) {
+      return super.responseError(req, res, error);
+    }
+  }
+  async putColaboradorById(req, res) {
+    try {
+      const id = req.params.id;
+      const body = req.body;
+      let response = await ColaboradoresRepository.putColaboradorById(id, body);
+      return super.responseSuccess(req, res, response);
+    } catch (error) {
+      return super.responseError(req, res, error);
+    }
+  }
+  async deleteColaboradorById(req, res) {
+    try {
+      const id = req.params.id;
+      let response = await ColaboradoresRepository.deleteColaboradorById(id);
+      return super.responseSuccess(req, res, response);
+    } catch (error) {
+      return super.responseError(req, res, error);
+    }
+  }
 }
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
 import Colaboradores from "../controllers/colaboradores.controller.js";
 
-
 const routes = Router();
 
 const colaboradores = new Colaboradores();
-
 
 //----------------------------------------------------------------//
 // ╔═╗╔═╗╔╦╗
@@ -13,7 +11,18 @@ const colaboradores = new Colaboradores();
 // ╚═╝╚═╝ ╩
 //----------------------------------------------------------------//
 
-routes.get(`/bioplan/colaboradores/get/all`,colaboradores.getColaboradores);
+// GET
+routes.get(`/bioplan/colaboradores/get/all`, colaboradores.getColaboradores);
+routes.get(`/bioplan/colaboradores/get/:id`, colaboradores.getColaboradoById);
 
+
+// POST
+routes.post(`/bioplan/colaboradores/post/colaborador`, colaboradores.postColaborador);
+
+// UPDATE
+routes.put(`/bioplan/colaboradores/put/:id`, colaboradores.putColaboradorById);
+
+// DELETE
+routes.delete(`/bioplan/colaboradores/delete/:id`, colaboradores.deleteColaboradorById);
 
 export default routes;
