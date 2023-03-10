e2191fb7b97b74bcc8e0dcc5cf4f8e1b44408f2e
HaroldUCSM <haroldosorio32@gmail.com>
2023-03-10 14:16:58 -0500

    se realizo las APIs de convocatorias

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import Colaboradores from "../controllers/colaboradores.controller.js";
+import Convocatorias from "../controllers/convocatorias.controller.js";
 import Noticias from "../controllers/noticias.controller.js";
 
 const routes = Router();
 
 const colaboradores = new Colaboradores();
 const noticias = new Noticias();
-
+const convocatorias = new Convocatorias();
 //----------------------------------------------------------------//
 // ╔═╗╔═╗╔╦╗
 // ║ ╦║╣  ║
@@ -22,6 +23,12 @@ routes.get(`/bioplan/colaboradores/get/:id`, colaboradores.getColaboradoById);
 routes.get(`/bioplan/noticias/get/all`, noticias.getNoticias);
 routes.get(`/bioplan/noticias/get/:id`, noticias.getNoticiasById);
 
+// ---> convocatorias
+routes.get(`/bioplan/convocatorias/get/all`, convocatorias.getConvocatorias);
+routes.get(
+  `/bioplan/convocatorias/get/:id`,
+  convocatorias.getConvocatoriasById
+);
 // POST
 // ---> colaboradores
 routes.post(
@@ -32,12 +39,20 @@ routes.post(
 // ---> noticias
 routes.post(`/bioplan/noticias/post/noticia`, noticias.postNoticias);
 
+// ---> convocatorias
+routes.post(
+  "/bioplan/convocatorias/post/convocatoria",
+  convocatorias.postConvocatorias
+);
 // UPDATE
 // ---> colaboradores
 routes.put(`/bioplan/colaboradores/put/:id`, colaboradores.putColaboradorById);
 
 // ---> noticias
 routes.put(`/bioplan/noticias/put/:id`, noticias.putNoticiasById);
+
+// ---> convocatorias
+routes.put(`/bioplan/convocatorias/put/:id`, convocatorias.putConvocatorias);
 
 // DELETE
 // ---> colaboradores
@@ -48,5 +63,11 @@ routes.delete(
 
 // ---> noticias
 routes.delete(`/bioplan/noticias/delete/:id`, noticias.deleteNoticiasById);
+
+// ---> convocatorias
+routes.delete(
+  `/bioplan/convocatorias/delete/:id`,
+  convocatorias.deleteConvocatorias
+);
 
 export default routes;
